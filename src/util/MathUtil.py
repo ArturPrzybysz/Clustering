0bb6489e74a4439bfedaa218f1192af35c714d3c
Artur Przybysz <Artur Przybysz>
2018-05-07 13:19:16 +0200

    util

--- src/util/MathUtil.py
+++ src/util/MathUtil.py
@@ -1,12 +1,17 @@
 import math
+import numpy as np
 
 
-def euclidean_distance(A, B):
-    inner_sum = 0
-    for a, b in zip(A, B):
-        inner_sum += pow(a - b, 2)
-    return pow(inner_sum, 0.5)
+def euclidean_distance(a: np.array, b: np.array):
+    return np.linalg.norm(a - b)
 
 
 def gaussian_function(x: float):
     return math.exp(-0.5 * x * x)
+
+
+def euclidean_normalization(matrix):
+    for vector in matrix:
+        length = euclidean_distance(vector, np.zeros_like(vector))
+        vector /= length
+    return matrix
